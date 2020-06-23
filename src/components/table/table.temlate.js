8d3cc23593841cb051782464db283384379fc2eb
TigranGrigoryan92 <vazgen.grigoryan@volo.global>
2020-06-23 14:47:10 +0400

    Finish table resize

--- src/components/table/table.temlate.js
+++ src/components/table/table.temlate.js
@@ -3,24 +3,29 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">
+    <div class="column" data-type="resizable" data-col="${index}">
       ${col}
+      <div class="col-resize" data-resize="col"></div>
     </div>
   `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row">
-      <div class="row-info">${index}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index || ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -40,7 +45,7 @@ export function createTable(rowsCount = 15) {
       .map(toColumn)
       .join('');
 
-  rows.push(createRow('', cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
